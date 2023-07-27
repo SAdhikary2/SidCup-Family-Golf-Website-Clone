81caaec865d94ca280a734419683a5bd8a5db54d
Sukalyan <sadhikary0055@gmail.com>
2023-07-28 01:44:17 +0530

    commit

--- script.js
+++ script.js
@@ -33,3 +33,73 @@ gsap.to("#main",{
 
     }
 })
+
+//For navber mouse hover
+const h4=document.querySelectorAll('#nav h4');
+h4.forEach(function(elem){
+     elem.addEventListener("mouseenter",function(){
+        crsr.style.scale=4;
+        crsr.style.border="0.5px solid #fff"
+        crsr.style.backgroundColor="transparent"
+     });
+     elem.addEventListener("mouseleave",function(){
+        crsr.style.scale=1;
+        crsr.style.border="0px solid #95c11e"
+        crsr.style.backgroundColor="#95c11e"
+     })
+});
+
+//About us page scroller animation
+gsap.from('#About-us img,#About-us-in',{
+    y:90,
+    opacity: 0,
+    duration:2,
+    stagger:1,
+    scrollTrigger:{
+        trigger:"#About-us",
+        scroller:'body',
+        start:"top 70%",
+        end:"top 50%",
+        // markers:true,
+        scrub:1
+    }
+});
+
+// card scroller animation
+gsap.from('.card',{
+    scale:0.8,
+    duration:0.5,
+    stagger:0.1,
+    scrollTrigger:{
+        trigger:".card",
+        scroller:'body',
+        start:"top 70%",
+        end:"top 65%",
+        scrub:1
+    }
+});
+
+//colon scroller animation
+gsap.from('#colon1',{
+    y:-70,
+    x:-70,
+    scrollTrigger:{
+        trigger:"#colon1",
+        scroller:"body",
+        start:"top 80%",
+        end:"top 70%",
+        scrub:4
+    }
+})
+
+gsap.from('#colon2',{
+    y:70,
+    x:70,
+    scrollTrigger:{
+        trigger:"#colon2",
+        scroller:"body",
+        start:"top 80%",
+        end:"top 70%",
+        scrub:4
+    }
+})
